a047dd184554b10eaf1b2da73268f7936f4796aa
anuyam <70201565+anuyam@users.noreply.github.com>
2020-08-28 10:26:40 +0530

    Add files via upload

---
+++ Random_number_generation.py
@@ -0,0 +1,11 @@
+import random
+random.seed(6)
+print(random.random())
+
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
